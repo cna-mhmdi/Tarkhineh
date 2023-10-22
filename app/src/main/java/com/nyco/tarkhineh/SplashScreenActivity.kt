009cc8d0bf18964539b3cc32ce0368d603bf4a47
cna <cna.mhmdi0435@gmail.com>
2023-10-22 16:32:01 +0330

    splashScreen added

---
+++ app/src/main/java/com/nyco/tarkhineh/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.nyco.tarkhineh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+
+}
