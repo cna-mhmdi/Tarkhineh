265eab6e8806562918fa84aff1e4bb23df0b7089
cna <cna.mhmdi0435@gmail.com>
2023-10-28 21:23:28 +0330

    CircleProgressBar added

--- app/src/main/java/com/nyco/tarkhineh/OnboardingActivity.kt
+++ app/src/main/java/com/nyco/tarkhineh/OnboardingActivity.kt
@@ -1,12 +1,20 @@
 package com.nyco.tarkhineh
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
 import androidx.viewpager.widget.ViewPager
 import com.nyco.tarkhineh.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private var currentProgress = 40
+
+    private val maxProgress = 120
+    private val animationDuration = 500L
+    private lateinit var progressAnimator: ValueAnimator
 
     private lateinit var binding : ActivityOnboardingBinding
 
@@ -17,11 +25,24 @@ class OnboardingActivity : AppCompatActivity() {
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        progressAnimator = ValueAnimator.ofInt(0, maxProgress).apply {
+            duration = animationDuration
+            interpolator = LinearInterpolator()
+            addUpdateListener { animation ->
+                currentProgress = animation.animatedValue as Int
+                binding.circleProgressBar.setProgress(currentProgress)
+            }
+        }
+
         viewPagerAdapter = ViewPagerAdapter(this)
         binding.onBoardingViewPager.adapter = viewPagerAdapter
 
-        binding.onBoardingBtn.setOnClickListener {
+        binding.circleButton.setOnClickListener {
             if (getItem(0) < 2) {
+                if (!progressAnimator.isRunning) {
+                    progressAnimator.setIntValues(currentProgress, currentProgress + 40)
+                    progressAnimator.start()
+                }
                 binding.onBoardingViewPager.setCurrentItem(getItem(1), true)
             } else {
                 val intent = Intent(this@OnboardingActivity, MainActivity::class.java)
---
+++ app/src/main/java/com/nyco/tarkhineh/ui/CircularProgressBar.kt
@@ -0,0 +1,56 @@
+package com.nyco.tarkhineh.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Color.GRAY
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class CircularProgressBar(context: Context, attr:AttributeSet): View(context,attr) {
+
+    private val paint: Paint = Paint()
+
+    private val borderPaint: Paint = Paint()
+    private var progress = 40
+    private val maxProgress = 120
+
+
+
+    init {
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 5f
+        paint.color = 0xFF417F56.toInt()
+
+        borderPaint.isAntiAlias = true
+        borderPaint.style = Paint.Style.STROKE
+        borderPaint.strokeWidth = 1f
+        borderPaint.color = Color.GRAY
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val centerX = width / 2
+        val centerY = height / 2
+        val radius = (centerX.coerceAtMost(centerY) - paint.strokeWidth / 2)
+        val startAngle = -90f
+        val sweepAngle = 360 * progress / maxProgress
+
+        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), radius + 1f / 2, borderPaint)
+
+        canvas.drawArc(
+            centerX - radius,
+            centerY - radius,
+            centerX + radius,
+            centerY + radius,
+            startAngle, sweepAngle.toFloat(),
+            false, paint)
+    }
+
+    fun setProgress(progress: Int) {
+        this.progress = progress
+        invalidate()
+    }
+}
