04d297a579e908420cd65b9254bec0c717dfe713
cna <cna.mhmdi0435@gmail.com>
2023-10-30 22:21:14 +0330

    VerifyCodeActivity.kt added

---
+++ app/src/main/java/com/nyco/tarkhineh/VerifyCodeActivity.kt
@@ -0,0 +1,11 @@
+package com.nyco.tarkhineh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifyCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_code)
+    }
+}
