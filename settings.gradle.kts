8f806f7868534a464c2f8992e33e2de0322c2a5a
cna <cna.mhmdi0435@gmail.com>
2023-11-18 18:47:04 +0330

    jitpack added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+
+        maven { url = uri("https://jitpack.io") }
     }
 }
 dependencyResolutionManagement {
