0dd8d43e39188ebfad09e9e7c6bbf5cf83866b27
cna <cna.mhmdi0435@gmail.com>
2023-11-20 12:24:27 +0330

    toolbar added

--- app/src/main/java/com/nyco/tarkhineh/fragments/HomeFragment.kt
+++ app/src/main/java/com/nyco/tarkhineh/fragments/HomeFragment.kt
@@ -1,20 +1,38 @@
 package com.nyco.tarkhineh.fragments
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.nyco.tarkhineh.R
+import com.nyco.tarkhineh.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity).setSupportActionBar(binding.mainToolbar)
+        (activity as AppCompatActivity).supportActionBar?.title = null
+
+        return binding.root
     }
 
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
