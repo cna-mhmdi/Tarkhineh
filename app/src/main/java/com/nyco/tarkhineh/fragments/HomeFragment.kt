ee4eb2a73a4d79510d0d6d479f910d891fc808b8
cna <cna.mhmdi0435@gmail.com>
2023-11-23 14:42:45 +0330

    scrollView for menu added

--- app/src/main/java/com/nyco/tarkhineh/fragments/HomeFragment.kt
+++ app/src/main/java/com/nyco/tarkhineh/fragments/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.nyco.tarkhineh.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
 import com.nyco.tarkhineh.R
 import com.nyco.tarkhineh.databinding.FragmentHomeBinding
+import com.nyco.tarkhineh.ktx.PrivacyActivity
 
 class HomeFragment : Fragment() {
 
@@ -34,6 +38,12 @@ class HomeFragment : Fragment() {
         imageList.add(SlideModel(R.drawable.slider5))
         imageList.add(SlideModel(R.drawable.slider6))
         binding.mainImageSlider.setImageList(imageList, ScaleTypes.CENTER_INSIDE)
+        binding.layoutMainMenu.layoutMainCourse.setOnClickListener {
+//            startActivity(Intent(requireContext(),PrivacyActivity::class.java))
+        }
+        binding.layoutMainMenu.layoutAppetizer.setOnClickListener {  }
+        binding.layoutMainMenu.layoutDessert.setOnClickListener {  }
+        binding.layoutMainMenu.layoutDessert.setOnClickListener {  }
 
         return binding.root
     }
