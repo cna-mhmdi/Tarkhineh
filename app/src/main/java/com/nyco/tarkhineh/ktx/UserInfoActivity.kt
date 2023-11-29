79a4ddc9d947e00439f6e29ab82b71895f50e77e
cna <cna.mhmdi0435@gmail.com>
2023-11-29 18:29:27 +0330

    state check for birth date and observerOne added

--- app/src/main/java/com/nyco/tarkhineh/ktx/UserInfoActivity.kt
+++ app/src/main/java/com/nyco/tarkhineh/ktx/UserInfoActivity.kt
@@ -2,16 +2,23 @@ package com.nyco.tarkhineh.ktx
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
 import android.util.Patterns
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
 import com.nyco.tarkhineh.R
 import com.nyco.tarkhineh.TarkhinehApplication
+import com.nyco.tarkhineh.TarkhinehRepository
 import com.nyco.tarkhineh.TarkhinehViewModel
 import com.nyco.tarkhineh.databinding.ActivityUserInfoBinding
 import com.nyco.tarkhineh.model.UpdateUser
@@ -23,13 +30,20 @@ class UserInfoActivity : AppCompatActivity() {
 
     private var isButtonEnable: Boolean = false
 
+    private lateinit var editTexts: List<TextInputEditText>
+    private lateinit var tarkhinehRepository: TarkhinehRepository
+    private lateinit var tarkhinehViewModel: TarkhinehViewModel
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private var completeToken: String? = null
+
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val editTexts = listOf(
+        editTexts = listOf(
             binding.editTextFirstName,
             binding.editTextLastName,
             binding.editTextEmail,
@@ -37,6 +51,17 @@ class UserInfoActivity : AppCompatActivity() {
             binding.editTextBirthDay,
             binding.editTextDisplayName
         )
+
+        tarkhinehRepository = (application as TarkhinehApplication).tarkhinehRepository
+        tarkhinehViewModel = ViewModelProvider(this,object : ViewModelProvider.Factory{
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return TarkhinehViewModel(tarkhinehRepository) as T
+            }
+        })[TarkhinehViewModel::class.java]
+
+        sharedPreferences= this.getSharedPreferences("TOKENS", Context.MODE_PRIVATE)
+        val accessToken = sharedPreferences.getString("access_token", null)
+        completeToken = if (accessToken != null) "Bearer $accessToken" else null
 
         setSupportActionBar(binding.userInfoToolbar)
         supportActionBar?.title = null
@@ -48,17 +73,6 @@ class UserInfoActivity : AppCompatActivity() {
             it.isEnabled = false
             it.isFocusableInTouchMode = false
         }
-
-        val tarkhinehRepository = (application as TarkhinehApplication).tarkhinehRepository
-        val tarkhinehViewModel = ViewModelProvider(this,object : ViewModelProvider.Factory{
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return TarkhinehViewModel(tarkhinehRepository) as T
-            }
-        })[TarkhinehViewModel::class.java]
-
-        val sharedPreferences = this.getSharedPreferences("TOKENS", Context.MODE_PRIVATE)
-        val accessToken = sharedPreferences?.getString("access_token", null)
-        val completeToken = if (accessToken != null) "Bearer $accessToken" else null
 
         tarkhinehViewModel.getUsersDetail(completeToken!!)
 
@@ -73,55 +87,94 @@ class UserInfoActivity : AppCompatActivity() {
 
         binding.editInfo.setOnClickListener {
             isButtonEnable = !isButtonEnable
+
             if (isButtonEnable) {
-                binding.editInfo.text = "اعمال تغییرات"
-                editTexts.forEach {
-                    it.isEnabled = true
-                    it.isFocusableInTouchMode = true
-                }
+                enterEditMode()
             } else {
-                val email = editTexts[2].text.toString().trim()
-                if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                    editTexts[2].error = "ایمیل معتبر وارد کنید!"
+                if (!validateEmail() || !validatePhone() || !validateBirthDate()) {
                     isButtonEnable = true
-                }else{
-                    val phonePattern = "^09\\d{9}$".toRegex()
-                    if (!phonePattern.matches(editTexts[3].text.toString().trim())){
-                        editTexts[3].error = "شماره همراه معتبر وارد کنید!"
-                        isButtonEnable = true
-                    } else{
-                        binding.editInfo.text = "ویرایش اطلاعات"
-                        editTexts.forEach {
-                            it.isEnabled = false
-                            it.isFocusableInTouchMode = false
-                        }
-
-                        val updateUser = UpdateUser(
-                            editTexts[0].text.toString(),
-                            editTexts[1].text.toString(),
-                            editTexts[2].text.toString(),
-                            editTexts[4].text.toString(),
-                            editTexts[5].text.toString(),
-                        )
-
-                        tarkhinehViewModel.updateUserDetail(completeToken,updateUser)
-
-                        tarkhinehViewModel.updateUser.observe(this){ updateUser->
-                            Toast.makeText(this, "تغییرات اعمال شد", Toast.LENGTH_SHORT).show()
-
-                            val sharedPreferencesNickname = this.getSharedPreferences("NICKNAME", Context.MODE_PRIVATE)
-                            val editor = sharedPreferencesNickname.edit()
-                            editor.putString("NickName", binding.editTextDisplayName.text.toString())
-                            editor.apply()
-                        }
-
-                        tarkhinehViewModel.getUpdateUserError().observe(this){ error->
-                            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
-                        }
-                    }
+                } else {
+                    applyChanges()
                 }
             }
         }
+    }
+
+    private fun enterEditMode() {
+        binding.editInfo.text = "اعمال تغییرات"
+        editTexts.forEach {
+            it.isEnabled = true
+            it.isFocusableInTouchMode = true
+        }
+    }
+
+    private fun validateEmail(): Boolean {
+        val email = editTexts[2].text.toString().trim()
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTexts[2].error = "ایمیل معتبر وارد کنید!"
+            return false
+        }
+        return true
+    }
+
+    private fun validatePhone(): Boolean {
+        val phone = editTexts[3].text.toString().trim()
+        if (!phone.matches("^09\\d{9}$".toRegex())) {
+            editTexts[3].error = "شماره همراه معتبر وارد کنید!"
+            return false
+        }
+        return true
+    }
+
+    private fun validateBirthDate(): Boolean {
+        val birthDate = editTexts[4].text.toString().trim()
+        if (!birthDate.matches("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$".toRegex())) {
+            editTexts[4].error = "تاریخ تولد معتبر وارد کنید! مثال 09-09-1379"
+            return false
+        }
+        return true
+    }
+
+    private fun applyChanges() {
+        binding.editInfo.text = "ویرایش اطلاعات"
+        editTexts.forEach {
+            it.isEnabled = false
+            it.isFocusableInTouchMode = false
+        }
+
+        val updateUser = UpdateUser(
+            editTexts[0].text.toString(),
+            editTexts[1].text.toString(),
+            editTexts[2].text.toString(),
+            editTexts[4].text.toString(),
+            editTexts[5].text.toString(),
+        )
+
+        tarkhinehViewModel.updateUserDetail(completeToken!!, updateUser)
+
+        tarkhinehViewModel.updateUser.observeOnce(this) { updateUser ->
+
+                Toast.makeText(this, "تغییرات اعمال شد", Toast.LENGTH_SHORT).show()
+
+                val sharedPreferencesNickname = this.getSharedPreferences("NICKNAME", Context.MODE_PRIVATE)
+                val editor = sharedPreferencesNickname.edit()
+                editor.putString("NickName", binding.editTextDisplayName.text.toString())
+                editor.apply()
+
+        }
+
+        tarkhinehViewModel.getUpdateUserError().observe(this) { error ->
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun <T> LiveData<T>.observeOnce(owner: LifecycleOwner, observer: (T?) -> Unit) {
+        observe(owner, object : Observer<T> {
+            override fun onChanged(t: T) {
+                removeObserver(this)
+                observer(t)
+            }
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
