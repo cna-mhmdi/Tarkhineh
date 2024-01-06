c014c7ba14f3eb90a57b84cba45af194db2da09d
cna <cna.mhmdi0435@gmail.com>
2024-01-06 19:31:58 +0330

    PaymentResActivity added

---
+++ app/src/main/java/com/nyco/tarkhineh/ktx/PaymentResActivity.kt
@@ -0,0 +1,12 @@
+package com.nyco.tarkhineh.ktx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nyco.tarkhineh.R
+
+class PaymentResActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_res)
+    }
+}
