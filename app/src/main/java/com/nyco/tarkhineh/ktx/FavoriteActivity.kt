0721c66a315593f2c3e71d5ef8df44c16b716ee7
cna <cna.mhmdi0435@gmail.com>
2024-01-08 18:19:18 +0330

    favorite added

---
+++ app/src/main/java/com/nyco/tarkhineh/ktx/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.nyco.tarkhineh.ktx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nyco.tarkhineh.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
